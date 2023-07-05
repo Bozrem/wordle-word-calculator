40ebe1f6eef6841a92ea63aeaaa6614344ea8b17
boomerboss <73359759+boomerboss@users.noreply.github.com>
2023-07-04 20:35:21 -0400

    Code Cleanup in Main.java and Guess.java

--- Guess.java
+++ Guess.java
@@ -1,8 +1,5 @@
 package org.example;
 
-/*
-/ Guess is what will be used in main, and runs a bunch of Game instances
- */
 
 public class Guess {
     private final Game[] games;
@@ -12,24 +9,18 @@ public class Guess {
         for (int i = 0; i < games.length; i++) {
             games[i] = new Game(guess, endWords[i], endWords);
         } // initialize the games
-    } // constructor
-
-    /*
-    /   This is more of a brute force method to getting data to ensure that it's accurate. I have it running the guess
-    /   with each possible result, and then based on the colors returned from the guess, it sorts through the available
-    /   words to see which ones are still possible answers
-     */
+    }
 
     public void runGames() {
-        for (Game g : games) {
-            g.run();
+        for (Game game : games) {
+            game.run();
         }
     }
 
     public double getPercentRemoved() {
         double sum = 0;
-        for (Game g : games) {
-            sum += g.getScore();
+        for (Game game : games) {
+            sum += game.getScore();
         }
         return sum / games.length;
     }
--- Main.java
+++ Main.java
@@ -9,47 +9,83 @@ import java.util.Scanner;
 
 
 public class Main {
+    static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        printIntro();
+        boolean gameLoop = true;
+        while (gameLoop) {
+            System.out.println("\nEnter the word to score. Must contain 5 alphabetical letters");
+            String guess = getGuess();
+            System.out.println(getScore(guess));
+            gameLoop = continuePlaying();
+        }
+        printOutro();
+    }
+
     public static String[] readEndWords() throws IOException {
         Path path = Path.of("endWords.txt").toAbsolutePath();
         List<String> endWordLines = Files.readAllLines(path, Charset.defaultCharset());
         return endWordLines.toArray(new String[0]);
-    } // translates endWords.txt into an array
+    }
 
-
-    public static void main(String[] args) throws IOException {
-
-        String[] endWords = readEndWords();
-        Scanner scan = new Scanner(System.in);
+    private static void printIntro() {
         System.out.println("*****************************************\n* Welcome to the Wordle Stat simulator! *");
         System.out.println("\nThis program works by running your guess with every answer that Wordle has to offer.");
         System.out.println("It then takes the colors you would have generated from the guess to rule out the answers that don't work");
         System.out.println("and creates a score for what percent of the answers it eliminated in that game, and returns the average for all games!");
-        while (true) {
-            System.out.println("\nEnter the word to score. Must contain 5 alphabetical letters");
+    }
 
-            String str = scan.nextLine().toLowerCase();
-            boolean flag = false;
-            for (int i = 0; i < str.length(); i++) {
-                if (!"abcdefghijklmnopqrstuvwxyz".contains(String.valueOf(str.charAt(i)))) {
-                    System.out.println("Invalid characters!");
-                    flag = true;
-                }
-            }
-            double percent;
-            if (str.length() != 5 || flag) System.out.println(str + " is not a valid entry!");
-            else {
-                Guess guess = new Guess(str, endWords);
-                guess.runGames();
-                percent = guess.getPercentRemoved();
-                System.out.println((Math.round(percent * 100.0)) / 100.0);
-            }
-            System.out.println("Would you like to try again? [Y/N]");
-            String prompt = scan.nextLine().toLowerCase();
-            if (prompt.equals("n")) {
-                break;
+    private static String getGuess() {
+        String str = "";
+        boolean guessLoop = true;
+        while (guessLoop) {
+            str = scan.nextLine().toLowerCase();
+            guessLoop = !isValidGuess(str);
+        }
+        return str;
+    }
+
+    private static boolean isValidGuess(String guess) {
+        if (isValidGuessLength(guess) && isValidGuessCharacters(guess)) {
+            return true;
+        }
+        System.out.println("Invalid Entry! Please try again");
+        return false;
+    }
+
+    private static boolean isValidGuessCharacters(String guess) {
+        for (int i = 0; i < guess.length(); i++) {
+            if (!"abcdefghijklmnopqrstuvwxyz".contains(String.valueOf(guess.charAt(i)))) {
+                System.out.println("Invalid characters!");
+                return false;
             }
         }
-        System.out.println("*****************************************\nThank you for playing!\nGame created by Bozrem\n*****************************************");
-
+        return true;
     }
+
+    private static boolean isValidGuessLength(String guess) {
+        return guess.length() == 5;
+    }
+
+    private static double getScore(String guess) throws IOException {
+        String[] endWords = readEndWords();
+        Guess gameGuess = new Guess(guess, endWords);
+        gameGuess.runGames();
+        return Math.round(gameGuess.getPercentRemoved() * 100.0) / 100.0;
+    }
+
+    private static boolean continuePlaying() {
+        System.out.println("Would you like to try again? [Y/N]");
+        String prompt = scan.nextLine().toLowerCase();
+        return prompt.equals("y");
+    }
+
+    private static void printOutro() {
+        System.out.println("*****************************************");
+        System.out.println("Thank you for playing!\nSoftware created by Bozrem");
+        System.out.println("*****************************************");
+    }
+
+
 }
